a344c4948419a3dc94928be3e021a6f0073798ec
Nuria Robledo <nuriarobledo0@gmail.com>
2022-10-14 11:19:11 -0300

    clase viernes 14/10

---
+++ modulo2-JavaScript/clase15/ejemplo1.js
@@ -0,0 +1,9 @@
+const parrafo = document.querySelector("p");
+
+const parrafos = document.querySelectorAll("p"); 
+
+console.log(parrafo); //me devuelve la primer p
+console.log(parrafos); //m devolvio un array d elementos
+
+const destacados = document.querySelectorAll(".destacados");
+console.log(destacados);
---
+++ modulo2-JavaScript/clase15/ejemplo4.js
@@ -0,0 +1,10 @@
+function todoslosParrafos (){
+    const allparrafos= document.getElementsByTagName('p'); //queryselectorall
+    console.log(allparrafos);
+
+    const num = allparrafos.length; // 7
+    //alert(`hay ${num} elementos <p< dentro de este document`);
+    alert('la suma de los p es' + allparrafos.length);
+}
+todoslosParrafos()
+
