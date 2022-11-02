4fcab5ecee85fdbc05c37dc415936fb18cf726fc
Nuria Robledo <nuriarobledo0@gmail.com>
2022-11-02 11:50:43 -0300

    clase 31/10

---
+++ modulo3-ReactJS/clase22/src/components/layout/Header.js
@@ -0,0 +1,10 @@
+const Header = (props) =>{
+    return(
+        <header>
+        <p> Hola soy el Header </p>
+        <p> lorem dimsjovdnjvanfvijvnskjkvsdvkj</p>
+        <p> ldaojfnnsoovndnijdsnkxcnsnfonasklacnlaenclanefl </p>
+        </header>
+    )
+}
+export default Header; 
